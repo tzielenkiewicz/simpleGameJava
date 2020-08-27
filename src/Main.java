952b9aaf223dabe0682711626cac205d060d29bc
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2020-08-27 11:49:08 +0200

    move and display board after move features

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,18 @@ public class Main {
         System.out.print("Hello, " + currentPlayer + ", it is time to make your move! Choose 1-9: ");
         int chosenFieldNumber = fieldNumber.nextInt();
         board[chosenFieldNumber - 1] = currentPlayer;
+        displayBoard(board);
 
+
+
+    }
+
+    private static void displayBoard(char[] board) {
+        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3");
+        System.out.println("-----------");
+        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6");
+        System.out.println("-----------");
+        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9");
     }
 
 
