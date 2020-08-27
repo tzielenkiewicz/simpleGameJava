7b221851043d70a9f1e2393fc300310f5c4eb666
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2020-08-27 10:43:28 +0200

    empty game board made

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println("-----------");
+        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println("-----------");
+        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8]);
+
+
+    }
+}
