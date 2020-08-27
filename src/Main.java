e7ba659a024e01d53b9df375d837b3401ee202dd
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2020-08-27 11:11:06 +0200

    empty game board made

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2]);
+        char[] board = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        System.out.println(" " + board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3");
         System.out.println("-----------");
-        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println(" " + board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6");
         System.out.println("-----------");
-        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8]);
+        System.out.println(" " + board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9");
 
+        char currentPlayer = 'X';
+
+        Scanner fieldNumber = new Scanner(System.in);
+        System.out.println();
+        System.out.print("Hello, " + currentPlayer + ", it is time to make your move! Choose 1-9: ");
+        int chosenFieldNumber = fieldNumber.nextInt();
+        board[chosenFieldNumber - 1] = currentPlayer;
 
     }
+
+
 }
